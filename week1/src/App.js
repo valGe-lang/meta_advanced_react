0e306d2a6eee11dd0f1b0196a04b9fd936e1f57d
Valtteri Göös <valtterigoos@gmail.com>
2023-09-25 13:01:40 +0300

    rest of week1 exercises&labs, template for week2 added

--- week1/src/App.js
+++ week1/src/App.js
@@ -2,7 +2,93 @@ import './App.css';
 import DessertsList from "./components/DessertsList";
 import { useState } from "react";
 import { validateEmail } from "./utils";
+import Switch from "./Switch";
+import { ThemeProvider, useTheme } from './ThemeContext';
 
+const Title = ({ children }) => {
+  const { theme } = useTheme();
+  return (
+    <h2
+      style={{
+        color: theme === "light" ? "black" : "white",
+      }}
+    >
+      {children}
+    </h2>
+  );
+};
+
+const Paragraph = ({ children }) => {
+  const { theme } = useTheme();
+  return (
+    <p
+      style={{
+        color: theme === "light" ? "black" : "white",
+      }}
+    >
+      {children}
+    </p>
+  );
+};
+
+const Content = () => {
+  return (
+    <div>
+      <Paragraph>
+        We are a pizza loving family. And for years, I searched and searched and
+        searched for the perfect pizza dough recipe. I tried dozens, or more.
+        And while some were good, none of them were that recipe that would
+        make me stop trying all of the others.
+      </Paragraph>
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <header>
+      <Title>Little Lemon 🍕</Title>
+      <Switch />
+    </header>
+  );
+};
+
+const Page = () => {
+  return (
+    <div className="Page">
+      <Title>When it comes to dough</Title>
+      <Content />
+    </div>
+  );
+};
+
+function App() {
+  const { theme } = useTheme();
+  return (
+    <div
+      className="App"
+      style={{
+        backgroundColor: theme === "light" ? "white" : "black",
+      }}
+    >
+      <Header />
+      <Page />
+    </div>
+  );
+}
+
+function Root() {
+  return (
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  );
+}
+
+export default Root;
+
+
+/*
 const PasswordErrorMessage = () => {
   return (
     <p className="FieldError">Password should have at least 8 characters</p>
@@ -118,82 +204,83 @@ export default App;
 
 
 
-// const desserts = [
-//   {
-//     name: "Chocolate Cake",
-//     calories: 400,
-//     createdAt: "2022-09-01",
-//   },
-//   {
-//     name: "Ice Cream",
-//     calories: 200,
-//     createdAt: "2022-01-02",
-//   },
-//   {
-//     name: "Tiramisu",
-//     calories: 300,
-//     createdAt: "2021-10-03",
-//   },
-//   {
-//     name: "Cheesecake",
-//     calories: 600,
-//     createdAt: "2022-01-04",
-//   },
-//   {
-//     name: "Hamburger",
-//     calories: "499",
-//     createdAt: "2022-1-05"
-//   },
-// ];
+const desserts = [
+  {
+    name: "Chocolate Cake",
+    calories: 400,
+    createdAt: "2022-09-01",
+  },
+  {
+    name: "Ice Cream",
+    calories: 200,
+    createdAt: "2022-01-02",
+  },
+  {
+    name: "Tiramisu",
+    calories: 300,
+    createdAt: "2021-10-03",
+  },
+  {
+    name: "Cheesecake",
+    calories: 600,
+    createdAt: "2022-01-04",
+  },
+  {
+    name: "Hamburger",
+    calories: "499",
+    createdAt: "2022-1-05"
+  },
+];
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <h2>List of low calorie desserts:</h2>
-//       <DessertsList data={desserts} />
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div className="App">
+      <h2>List of low calorie desserts:</h2>
+      <DessertsList data={desserts} />
+    </div>
+  );
+}
 
-// export default App;
-
-
-// const items = [{
-//   id: "1",
-//   title: "Keyboard",
-//   description: "Good keyboard",
-//   image: "",
-//   price: "$100.00",
-// },
-// {
-//   id: "2",
-//   title: "Headset",
-//   description: "Good keyboard",
-//   image: "",
-//   price: "$75.00",
-// },
-// {
-//   id: "2",
-//   title: "Mouse",
-//   description: "Good keyboard",
-//   image: "",
-//   price: "$80.00",
-// }
-// ];
+export default App;
 
 
-// function App() {
-//   const listItems = items.map(items => {
-//     const itemText = `${items.title} - ${items.price}`
-//     return <li>{itemText}</li>
-//   })
-//   return (
-//     <div>
-//       <ul>
-//         {listItems}
-//       </ul>
-//     </div>
-//   );
-// }
+const items = [{
+  id: "1",
+  title: "Keyboard",
+  description: "Good keyboard",
+  image: "",
+  price: "$100.00",
+},
+{
+  id: "2",
+  title: "Headset",
+  description: "Good keyboard",
+  image: "",
+  price: "$75.00",
+},
+{
+  id: "2",
+  title: "Mouse",
+  description: "Good keyboard",
+  image: "",
+  price: "$80.00",
+}
+];
 
-// export default App;
+
+function App() {
+  const listItems = items.map(items => {
+    const itemText = `${items.title} - ${items.price}`
+    return <li>{itemText}</li>
+  })
+  return (
+    <div>
+      <ul>
+        {listItems}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+*/
